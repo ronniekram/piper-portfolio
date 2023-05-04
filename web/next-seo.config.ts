3670202f1429bf396b952adc4e766acd8be22b57
ronniekram <arynn.boniface@gmail.com>
2023-05-03 23:34:31 -0400

    working: fixes og image url

--- web/next-seo.config.ts
+++ web/next-seo.config.ts
@@ -14,7 +14,7 @@ const config: DefaultSeoProps = {
     description: `The design portfolio of Piper Olsen - a digital and print artist in Pittsburgh, PA`,
     images: [
       {
-        url: `/opengraph.png`,
+        url: `https://cdn.sanity.io/images/a34r16vo/production/5e1fdc281aa09aeeaf3cdbd9b42bd482fe5f8957-1200x630.png`,
         width: 1200,
         height: 630,
         alt: `Piper Olsen Design Portfolio Opengraph Image`,
